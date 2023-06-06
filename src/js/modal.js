90c5c789e3ec4d56e267ec14c175892a5ac742b5
Nataly-Naf <nat.spivak84@gmail.com>
2023-06-06 15:26:52 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,12 +16,10 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-subscribe-modal-open]'),
-    openSecModalBtn: document.querySelector('[data-subscribe-modal-open-sec]'),
     closeSubscrModalBtn: document.querySelector('[data-subscribe-modal-close]'),
     modal: document.querySelector('[data-subscribe-modal]'),
   };
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.openSecModalBtn.addEventListener('click', toggleModal);
   refs.closeSubscrModalBtn.addEventListener("click", toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
